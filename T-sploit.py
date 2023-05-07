4609b51b367dfdac93c255daff03d76782fbf413
rootexx <velies0625@gmail.com>
2023-05-07 22:36:06 +0300

    Update T-sploit.py

--- T-sploit.py
+++ T-sploit.py
@@ -1,49 +1,44 @@
-import requests
-import subprocess
+import os
 
 # IP tespiti için fonksiyon
 def get_website_ip(url):
-    try:
-        ip = socket.gethostbyname(url)
-        print("Website IP: ", ip)
-    except socket.gaierror:
-        print("Geçersiz URL.")
+    # IP tespiti kodları
+    pass
 
 # Admin paneli bulma için fonksiyon
 def find_admin_panel(url):
-    admin_panel_paths = ["admin", "login", "dashboard", "admin-panel"]
-    
-    for path in admin_panel_paths:
-        admin_url = url + "/" + path
-        response = requests.get(admin_url)
-        if response.status_code == 200:
-            print("Admin Panel URL: ", admin_url)
-            break
+    # Admin paneli bulma kodları
+    pass
 
 # DDoS saldırısı için fonksiyon
-def perform_ddos_attack():
-    subprocess.call(["python3", "ddos.py"])
+def perform_ddos_attack(url):
+    # DDoS saldırısı kodları
+    os.system("python3 ddos.py")  # ddos.py dosyasını çalıştır
 
 # Ana işlev
 def main():
-    print("[1] Website IP'sini bul")
-    print("[2] Admin panelini bul")
-    print("[3] DDoS saldırısı gerçekleştir")
-    print("[Q] Çıkış")
+    while True:
+        print("[1] Website IP'sini bul")
+        print("[2] Admin panelini bul")
+        print("[3] DDoS saldırısı gerçekleştir")
+        print("[Q] Çıkış")
 
-    choice = input("Seçiminizi yapın: ")
+        choice = input("Seçim yapın: ")
 
-    if choice == "1":
-        url = input("Website URL'sini girin: ")
-        get_website_ip(url)
-    elif choice == "2":
-        url = input("Website URL'sini girin: ")
-        find_admin_panel(url)
-    elif choice == "3":
-        perform_ddos_attack()
-    elif choice.lower() == "q":
-        return
-    else:
-        print("Geçersiz bir seçim yaptınız. Tekrar deneyin.")
+        if choice == "1":
+            url = input("Website URL'sini girin: ")
+            get_website_ip(url)
+        elif choice == "2":
+            url = input("Website URL'sini girin: ")
+            find_admin_panel(url)
+        elif choice == "3":
+            url = input("lütfen enter'a basın ")
+            perform_ddos_attack(url)
+            break  # DDoS saldırısı gerçekleştirdikten sonra döngüden çık
+        elif choice.lower() == "q":
+            break
+        else:
+            print("Geçersiz bir seçim yaptınız. Tekrar deneyin.")
 
+if __name__ == "__main__":
     main()
