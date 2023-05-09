03d6f66eec983d82d4f43a8aff2364b6a717e774
rootexx <velies0625@gmail.com>
2023-05-09 21:40:01 +0300

    update

--- T-sploit.py
+++ T-sploit.py
@@ -13,7 +13,12 @@ def find_admin_panel(url):
 # DDoS saldırısı için fonksiyon
 def perform_ddos_attack(url):
     # DDoS saldırısı kodları
-    os.system("python3 ddos.py")  # ddos.py dosyasını çalıştır
+    os.system("python3 ddos.py")
+
+# SQL injection için fonksiyon
+def perform_sql_injection(url):
+    # SQL injection kodları
+    os.system("python3 sqlmap.py -u " + url)  # sqlmap.py dosyasını çalıştır
 
 # Ana işlev
 def main():
@@ -21,6 +26,7 @@ def main():
         print("[1] Website IP'sini bul")
         print("[2] Admin panelini bul")
         print("[3] DDoS saldırısı gerçekleştir")
+        print("[4] SQL injection gerçekleştir")
         print("[Q] Çıkış")
 
         choice = input("Seçim yapın: ")
@@ -32,9 +38,11 @@ def main():
             url = input("Website URL'sini girin: ")
             find_admin_panel(url)
         elif choice == "3":
-            url = input("lütfen enter'a basın ")
+            url = input("Lütfen Enter'a Basın ")
             perform_ddos_attack(url)
-            break  # DDoS saldırısı gerçekleştirdikten sonra döngüden çık
+        elif choice == "4":
+            url = input("Lütfen Enter'a Basın ")
+            perform_sql_injection(url)
         elif choice.lower() == "q":
             break
         else:
